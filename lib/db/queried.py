c63a18fe234db3cb839ce56b7117ac32c4a9a13a
David Probst <david.probst@tum.de>
2021-08-10 15:00:11 +0200

    Updated true start and end dates

--- lib/db/queried.py
+++ lib/db/queried.py
@@ -8,7 +8,7 @@ QUERIES = {
         'data_start_date': day_wrapping_datetimes(datetime.strptime("2021-04-09", '%Y-%m-%d'))[0],
         'data_end_date': day_wrapping_datetimes(datetime.strptime("2021-05-29", '%Y-%m-%d'))[1],
         'true_start_date': day_wrapping_datetimes(datetime.strptime("2021-04-13", '%Y-%m-%d'))[0],
-        'true_end_date': day_wrapping_datetimes(datetime.strptime("2021-04-23", '%Y-%m-%d'))[1]
+        'true_end_date': day_wrapping_datetimes(datetime.strptime("2021-04-15", '%Y-%m-%d'))[1]
         # eng waves happens between 17th and 23d
     },
     'amthor': {
@@ -23,7 +23,7 @@ QUERIES = {
         'data_start_date': day_wrapping_datetimes(datetime.strptime("2021-05-28", '%Y-%m-%d'))[0],
         'data_end_date': day_wrapping_datetimes(datetime.strptime("2021-06-08", '%Y-%m-%d'))[1],
         'true_start_date': day_wrapping_datetimes(datetime.strptime("2021-06-04", '%Y-%m-%d'))[0],
-        'true_end_date': None
+        'true_end_date': day_wrapping_datetimes(datetime.strptime("2021-06-06", '%Y-%m-%d'))[1],
     },
     'masken_csu': {
         'query': '#Nuesslein OR #Maskendeals OR #Maskenaffäre OR #Aserbaidschan OR #schwarzerFilz',
@@ -37,7 +37,7 @@ QUERIES = {
         'data_start_date': day_wrapping_datetimes(datetime.strptime("2021-04-28", '%Y-%m-%d'))[0],
         'data_end_date': day_wrapping_datetimes(datetime.strptime("2021-05-15", '%Y-%m-%d'))[1],
         'true_start_date': day_wrapping_datetimes(datetime.strptime("2021-05-05", '%Y-%m-%d'))[0],
-        'true_end_date': day_wrapping_datetimes(datetime.strptime("2021-05-09", '%Y-%m-%d'))[1],
+        'true_end_date': day_wrapping_datetimes(datetime.strptime("2021-05-08", '%Y-%m-%d'))[1],
     },
     'studierenWieBaerbock': {
         'query': '#studierenwieBaerbock OR #Bundescancelerin OR #baerbockfail',
@@ -51,22 +51,24 @@ QUERIES = {
                  'OR conversation_id:1210935523545620480',
         'data_start_date': day_wrapping_datetimes(datetime.strptime("2019-12-21", '%Y-%m-%d'))[0],
         'data_end_date': day_wrapping_datetimes(datetime.strptime("2020-01-07", '%Y-%m-%d'))[1],
-        'true_start_date': None,
-        'true_end_date': None
+        'true_start_date': day_wrapping_datetimes(datetime.strptime("2019-12-28", '%Y-%m-%d'))[0],
+        'true_end_date': day_wrapping_datetimes(datetime.strptime("2020-01-01", '%Y-%m-%d'))[1]
     },
+    '''
     'helmeLookLikeShit': {
         'query': '#HelmeRettenLeben OR #lookslikeshit OR #saveslifes OR conversation_id:1108842805089177615',
-        'data_start_date': day_wrapping_datetimes(datetime.strptime("2019-05-14", '%Y-%m-%d'))[0],
-        'data_end_date': day_wrapping_datetimes(datetime.strptime("2019-05-31", '%Y-%m-%d'))[1],
+        'data_start_date': day_wrapping_datetimes(datetime.strptime("2019-03-14", '%Y-%m-%d'))[0],
+        'data_end_date': day_wrapping_datetimes(datetime.strptime("2019-03-31", '%Y-%m-%d'))[1],
         'true_start_date': None,
         'true_end_date': None
     },
+    '''
     'studentinnenfutter': {
         'query': '#Studentinnenfutter OR Studierendenfutter OR conversation_id:1413204084694437888',
         'data_start_date': day_wrapping_datetimes(datetime.strptime("2021-07-01", '%Y-%m-%d'))[0],
         'data_end_date': day_wrapping_datetimes(datetime.strptime("2021-07-18", '%Y-%m-%d'))[1],
-        'true_start_date': None,
-        'true_end_date': None
+        'true_start_date': day_wrapping_datetimes(datetime.strptime("2021-07-09", '%Y-%m-%d'))[0],
+        'true_end_date': day_wrapping_datetimes(datetime.strptime("2021-07-09", '%Y-%m-%d'))[1],
     },
     'dbUnternehmerischeEntscheidung': {
         'query': 'conversation_id:1359969795068727296 OR conversation_id:1349076313315942404 OR '
@@ -80,30 +82,30 @@ QUERIES = {
         'query': 'conversation_id:1206182673888219136',
         'data_start_date': day_wrapping_datetimes(datetime.strptime("2019-12-08", '%Y-%m-%d'))[0],
         'data_end_date': day_wrapping_datetimes(datetime.strptime("2019-12-25", '%Y-%m-%d'))[1],
-        'true_start_date': None,
-        'true_end_date': None
+        'true_start_date': day_wrapping_datetimes(datetime.strptime("2019-12-15", '%Y-%m-%d'))[0],
+        'true_end_date': day_wrapping_datetimes(datetime.strptime("2019-12-16", '%Y-%m-%d'))[1]
     },
     'kloecknerNestle': {
         'query': '(#Klöckner #Nestle) OR conversation_id:1135553266476040192',
         'data_start_date': day_wrapping_datetimes(datetime.strptime("2019-05-27", '%Y-%m-%d'))[0],
         'data_end_date': day_wrapping_datetimes(datetime.strptime("2019-06-13", '%Y-%m-%d'))[1],
-        'true_start_date': None,
-        'true_end_date': None
+        'true_start_date': day_wrapping_datetimes(datetime.strptime("2019-06-05", '%Y-%m-%d'))[0],
+        'true_end_date': day_wrapping_datetimes(datetime.strptime("2019-06-07", '%Y-%m-%d'))[1]
     },
     'uefaUngarn': {
         'query': '#MuenchenMachEsTrotzdem OR (#Regenbogen #UEFA) OR (#Regenbogenfarben #UEFA) OR (#AllianzArena #UEFA) '
                  'OR (#UEFA #GERHUN) OR (#Regenbogen #EURO2020) OR (#Regenbogenfarben #EURO2020)',
         'data_start_date': day_wrapping_datetimes(datetime.strptime("2021-06-14", '%Y-%m-%d'))[0],
         'data_end_date': day_wrapping_datetimes(datetime.strptime("2021-07-01", '%Y-%m-%d'))[1],
-        'true_start_date': None,
-        'true_end_date': None
+        'true_start_date': day_wrapping_datetimes(datetime.strptime("2021-06-20", '%Y-%m-%d'))[0],
+        'true_end_date': day_wrapping_datetimes(datetime.strptime("2021-06-23", '%Y-%m-%d'))[1]
     },
     'laschetLacht': {
         'query': '#Laschetlacht OR #LaschetRuecktritt OR #LautGegenLaschet OR conversation_id:1416449185172369411',
         'data_start_date': day_wrapping_datetimes(datetime.strptime("2021-07-10", '%Y-%m-%d'))[0],
         'data_end_date': day_wrapping_datetimes(datetime.strptime("2021-07-27", '%Y-%m-%d'))[1],
-        'true_start_date': None,
-        'true_end_date': None
+        'true_start_date': day_wrapping_datetimes(datetime.strptime("2021-07-17", '%Y-%m-%d'))[0],
+        'true_end_date': day_wrapping_datetimes(datetime.strptime("2021-07-19", '%Y-%m-%d'))[1]
     }
 }
 
@@ -113,6 +115,3 @@ for key in QUERIES:
     if 'true_end_date' not in QUERIES[key] or not QUERIES[key]['true_end_date']:
         QUERIES[key]['true_end_date'] = QUERIES[key]['data_end_date']
 
-
-if __name__ == "__main__":
-    print(QUERIES)
