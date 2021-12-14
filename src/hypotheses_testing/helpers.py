fb59f9db5d1374312599e6bafd953a695f6db332
David Probst <david.probst@tum.de>
2021-12-14 17:01:35 +0100

    basic hypothesis testing

---
+++ src/hypotheses_testing/helpers.py
@@ -0,0 +1,60 @@
+from json import loads
+
+import pandas as pd
+from mongoengine.queryset.visitor import Q
+
+from src.db.queried import QUERIES
+from src.db.schemes import Tweets
+
+from sklearn.preprocessing import LabelEncoder
+
+
+def load_hypothesis_dataset(
+    attributes: list[str] = ["is_offensive", "tweet_type", "user_type"]
+) -> pd.DataFrame:
+    tweets_collection = []
+    i = 0
+    for name, query_dict in QUERIES.items():
+        print(f"Loading {name}")
+        query = query_dict["query"]
+        firestorm_tweets_query_set = (
+            Tweets.objects(Q(search_params__query=query) & Q(lang="de"))
+            .only(*attributes)
+            .to_json()
+        )
+
+        firestorm_df = pd.DataFrame.from_records(loads(firestorm_tweets_query_set))
+
+        tweets_collection.append(firestorm_df)
+
+    return pd.concat(tweets_collection)
+
+
+def dummify_categoricals(firestorm_df: pd.DataFrame) -> pd.DataFrame:
+    firestorm_dummies = pd.concat(
+        [
+            firestorm_df,
+            pd.get_dummies(firestorm_df["tweet_type"]),
+            pd.get_dummies(firestorm_df["user_type"]),
+        ],
+        axis=1,
+    )
+
+    # drop one dummy var
+    firestorm_dummies = firestorm_dummies.drop(
+        ["retweet without comment", "laggard"], axis=1
+    )
+
+    aggr_enc = LabelEncoder()
+    firestorm_dummies["aggression_num"] = aggr_enc.fit_transform(
+        firestorm_df["is_offensive"]
+    )
+
+    firestorm_dummies = firestorm_dummies.drop(
+        ["is_offensive", "tweet_type", "user_type"], axis=1
+    )
+
+    firestorm_dummies.columns = firestorm_dummies.columns.str.replace(" ", "_")
+    firestorm_dummies.columns = firestorm_dummies.columns.str.replace("-", "_")
+
+    return firestorm_dummies
---
+++ src/hypotheses_testing/main.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from src.db.connection import connect_to_mongo
+from src.hypotheses_testing.helpers import (
+    load_hypothesis_dataset,
+    dummify_categoricals
+)
+
+
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+from scipy.stats import chi2_contingency
+
+connect_to_mongo()
+
+firestorm_df = load_hypothesis_dataset()
+
+print("=====AGGRESSION BY TWEET TYPE=====")
+
+# === Correlation Analysis
+firestorm_contingency_table = pd.crosstab(
+    firestorm_df["is_offensive"], firestorm_df["tweet_type"]
+)
+print(firestorm_contingency_table)
+stat, p, dof, expected = chi2_contingency(firestorm_contingency_table)
+
+print("CHI_SQUARED:")
+print(f"stat: {stat}:")
+print(f"p-value: {p}:")
+print(f"degrees of freedom: {dof}")
+print(f"expected: {expected}")
+
+
+# === LOG REG
+firestorm_dummies = dummify_categoricals(firestorm_df)
+
+print("====TWEET TYPE====")
+model_tweet_type = smf.glm(
+    formula="aggression_num ~ original_tweet + reply + retweet_with_comment",
+    family=sm.families.Binomial(link=sm.genmod.families.links.logit),
+    data=firestorm_dummies,
+).fit()
+
+print(model_tweet_type.summary())
+
+print("====USERS TYPE====")
+model_users = smf.glm(
+    formula="aggression_num ~ active + hyper_active",
+    family=sm.families.Binomial(link=sm.genmod.families.links.logit),
+    data=firestorm_dummies,
+).fit()
+
+print(model_users.summary())
